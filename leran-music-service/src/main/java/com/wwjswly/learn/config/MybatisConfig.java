8a33ed3ead9e2c6e2e2f2edcc39adedcb5a23531
布衣 <fa20091001@163.com>
2019-02-08 00:25:04 +0800

    add:添加网易用音乐微服务,添加zuul网关

---
+++ leran-music-service/src/main/java/com/wwjswly/learn/config/MybatisConfig.java
@@ -0,0 +1,16 @@
+package com.wwjswly.learn.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.wwjswly.learn.mapper")
+public class MybatisConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
---
+++ leran-music-service/src/main/java/com/wwjswly/learn/mapper/TUserMapper.java
@@ -0,0 +1,16 @@
+package com.wwjswly.learn.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wwjswly.learn.entity.TUser;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author zhangzhaofa
+ * @since 2018-12-15
+ */
+public interface TUserMapper extends BaseMapper<TUser> {
+
+}
