0bc5ec9b807ea96d8ca1e33af7b65cac45384cc4
布衣 <fa20091001@163.com>
2019-03-29 00:33:12 +0800

    不提交清单

--- learn-gateway/src/main/java/com/wwjswly/learn/gateway/GatewayApplication.java
+++ learn-gateway/src/main/java/com/wwjswly/learn/gateway/GatewayApplication.java
@@ -3,12 +3,14 @@ package com.wwjswly.learn.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class GatewayApplication {
 
     public static void main(String[] args) {
