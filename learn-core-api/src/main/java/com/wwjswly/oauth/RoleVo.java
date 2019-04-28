c45624aeb6bfa0bf6c7f669232e4f1751b5ae953
布衣 <fa20091001@163.com>
2019-04-28 22:48:46 +0800

    添加oauth2 以及config-service

---
+++ learn-core-api/src/main/java/com/wwjswly/oauth/RoleVo.java
@@ -0,0 +1,26 @@
+package com.wwjswly.oauth;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoleVo implements Serializable {
+    private static final long serialVersionUID = 2179037393108205286L;
+    private Integer id;
+
+    private String name;
+
+    private String value;
+
+    private String tips;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer status;
+}
