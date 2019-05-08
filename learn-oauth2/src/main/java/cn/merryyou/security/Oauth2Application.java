e3e9a7347a48046b6b901e2c0b3a78de0758dfba
JavaDer <fa20091001@163.com>
2019-05-08 22:19:23 +0800

    添加。gitignore文件

--- learn-oauth2/src/main/java/cn/merryyou/security/Oauth2Application.java
+++ learn-oauth2/src/main/java/cn/merryyou/security/Oauth2Application.java
@@ -1,23 +1,8 @@
 package cn.merryyou.security;
 
-import cn.merryyou.security.properties.OAuth2Properties;
-import cn.merryyou.security.utils.JsonUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 
 /**
  * Created by hxy on 2018/6/20
